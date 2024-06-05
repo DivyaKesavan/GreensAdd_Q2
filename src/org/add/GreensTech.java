c34d44d878978e678931be76a054a0db7c5136e3
DivyaKesavan <divikesav@gmail.com>
2024-06-05 16:00:31 +0530

    new code is added

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,39 @@
+package org.add;
+public class GreensTech {
+	
+		private void greensOmr()
+		{
+		System.out.println("greensOmr Method1");	
+		}
+		
+		private void greensAdayar()
+		{
+		System.out.println("greensAdayar Method2");	
+		}
+		
+		private void greensTambaram()
+		{
+		System.out.println("greensTambaram Method3");	
+		}
+		
+		private void greensVelacherry()
+		{
+		System.out.println("greensVelacherry Method4");	
+		}
+		
+		private void greensAnnaNagar()
+		{
+		System.out.println("greensAnnaNagar Method5");	
+		}		
+		public static void main(String[] args) {
+			
+			GreensTech f = new GreensTech();
+			f.greensOmr();
+			f.greensAdayar();
+			f.greensTambaram();
+			f.greensVelacherry();
+			f.greensAnnaNagar();
+		}
+
+
+}
